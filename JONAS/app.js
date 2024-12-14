bf05eafbc1b6e4d1874d472d204457c40d2c0e11
Nikesh <nikeshkumarsingh0987@gmail.com>
2024-12-14 10:07:58 +0530

    intersection

--- JONAS/app.js
+++ JONAS/app.js
@@ -1,13 +1,17 @@
-const setExm = new Set('pizzaapple');
+const italianFood = new Set(['Pizza', 'Pasta', 'Risotto', 'Spaghetti', 'Fusilli']);
+const maxicalianFood = new Set(['Pizza', 'Pasta', 'Risotto']);
 
-console.log(setExm.size); // 6
-console.log(setExm.has('p')); // true
-console.log(setExm.has('o')); // false
-console.log(setExm.add('o')); // { 'p', 'i', 'z', 'a', 'l', 'e', 'o' }
-console.log(setExm.delete('o')); // { 'p', 'i', 'z', 'a', 'l', 'e' }
-console.log(setExm.clear()); // {}
+const duplicated = italianFood.intersection(maxicalianFood);
+const unical = italianFood.union(maxicalianFood);
+const spread = [...italianFood, ...maxicalianFood];
+const difference = italianFood.difference(maxicalianFood);
+const symanticAll = italianFood.symmetricDifference(maxicalianFood);
 
+console.log(duplicated); // Set { 'Pizza', 'Pasta', 'Risotto' }
+console.log('unical : ',unical); // Set { 'Pizza', 'Pasta', 'Risotto', 'Spaghetti', 'Fusilli' }
+console.log('spread : ',spread); // [ 'Pizza', 'Pasta', 'Risotto', 'Spaghetti', 'Fusilli' ]
+console.log('difference : ',difference); // Set { 'Spaghetti', 'Fusilli' }
+console.log('symanticAll : ',symanticAll); // Set { 'Spaghetti', 'Fusilli', 'Pizza', 'Pasta', 'Risotto' }
 
-console.log(setExm);
 
 
--- JONAS/readMe.md
+++ JONAS/readMe.md
@@ -465,3 +465,24 @@ console.log(setExm.clear()); // {}
 
 console.log(setExm);
 ```
+# > Intersection
+
+- Find the duplicate values of the Set.
+
+```
+const italianFood = new Set(['Pizza', 'Pasta', 'Risotto', 'Spaghetti', 'Fusilli']);
+const maxicalianFood = new Set(['Pizza', 'Pasta', 'Risotto']);
+
+const duplicated = italianFood.intersection(maxicalianFood);
+const unical = italianFood.union(maxicalianFood);
+const spread = [...italianFood, ...maxicalianFood];
+const difference = italianFood.difference(maxicalianFood);
+const symanticAll = italianFood.symmetricDifference(maxicalianFood);
+
+console.log(duplicated); // Set { 'Pizza', 'Pasta', 'Risotto' }
+console.log('unical : ',unical); // Set { 'Pizza', 'Pasta', 'Risotto', 'Spaghetti', 'Fusilli' }
+console.log('spread : ',spread); // [ 'Pizza', 'Pasta', 'Risotto', 'Spaghetti', 'Fusilli' ]
+console.log('difference : ',difference); // Set { 'Spaghetti', 'Fusilli' }
+console.log('symanticAll : ',symanticAll); // Set { 'Spaghetti', 'Fusilli', 'Pizza', 'Pasta', 'Risotto' }
+
+```
