af711ea8e87e4ce17cc59f320a161ea745ab066b
Nikesh <nikeshkumarsingh0987@gmail.com>
2025-02-02 03:57:03 +0530

    apply and bind Example

--- JONAS/app.js
+++ JONAS/app.js
@@ -1,43 +1,26 @@
-const flight = {
-    company: "Lufthansa",
-    number: "LH123",
-    airbus: 333,
-    plane: "A320",
-    gate: "23B",
-    bookings: [],
-    checkIn: function(flightNum, name){
-       console.log(`${name} checked in to ${this.company} ${flightNum}`) 
-       flight.bookings.push({flight: `${this.company} ${flightNum}`, name: name,Gate: this.gate});
-    }
-}
-flight.checkIn(23, 'mohamed');
-flight.checkIn(24, 'ali');
-flight.checkIn(25, 'sara');
-flight.bookings.forEach((booking) => console.log(booking));
-
+let flightDetails = {
+  company: "Cargo Jet",
+  gate: 23,
+  bookings: [],
+  checkIn: function (flightNum, name) {
+    console.log(`${name} checked in to ${this.company} ${flightNum}`);
+    this.gate = this.gate === undefined ? 1 : this.gate + 1;
+    this.bookings.push({
+      flight: `${this.company} ${flightNum}`,
+      name: name,
+      Gate: this.gate,
+    });
+  },
+};
 const secondFlight = {
-    company: "Lufthansa",
-    number: "Lh321",
-    bookings: [],
+  company: "airline",
+  bookings: [],
+};
+const book = flightDetails.checkIn;
+const binAll = book.bind(secondFlight); //bind assign in a new function keyword
+let flightData = [23, "mohamed"]; //rapping by array
+book.apply(secondFlight, flightData);
+binAll(24, "ali");
 
-}
-//Assigning but this will not work
-const book = flight.checkIn;
-// book(89,'mohamed');  //Error typoError  
-book.call(secondFlight, 89,'john');
-book.call(secondFlight, 9,'smith');
-console.log(secondFlight.bookings);
-
-const flight1 = { name: "Lufthansa", code: "LH123" };
-const flight2 = { name: "Qatar Airways", code: "QR456" };
-
-function checkIn(passengerName) {
-    console.log(`${passengerName} checked in to flight ${this.name} (${this.code}).`);
-}
-
-// Arguments passed as an array
-checkIn.apply(flight1, ["Mohamed"]);
-// Output: Mohamed checked in to flight Lufthansa (LH123).
-
-checkIn.apply(flight2, ["Ali"]);
-// Output: Ali checked in to flight Qatar Airways (QR456).
+book.call(secondFlight, 9, "smith");
+console.log(secondFlight); // { company: 'airline', bookings: [ { flight: 'airline 24', name: 'ali', Gate: 2 } ] }
--- JONAS/readMe.md
+++ JONAS/readMe.md
@@ -679,3 +679,34 @@ book.call(secondFlight, 89,'john');
 book.call(secondFlight, 9,'smith');
 console.log(secondFlight.bookings);
 ```
+
+```
+//APPLY AND BIND EXAMPLE
+
+let flightDetails = {
+  company: "Cargo Jet",
+  gate: 23,
+  bookings: [],
+  checkIn: function (flightNum, name) {
+    console.log(`${name} checked in to ${this.company} ${flightNum}`);
+    this.gate = this.gate === undefined ? 1 : this.gate + 1;
+    this.bookings.push({
+      flight: `${this.company} ${flightNum}`,
+      name: name,
+      Gate: this.gate,
+    });
+  },
+};
+const secondFlight = {
+  company: "airline",
+  bookings: [],
+};
+const book = flightDetails.checkIn;
+const binAll = book.bind(secondFlight); //bind assign in a new function keyword
+let flightData = [23, "mohamed"]; //rapping by array
+book.apply(secondFlight, flightData);
+binAll(24, "ali");
+
+book.call(secondFlight, 9, "smith");
+console.log(secondFlight); // { company: 'airline', bookings: [ { flight: 'airline 24', name: 'ali', Gate: 2 } ] }
+```
